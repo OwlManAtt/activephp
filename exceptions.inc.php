0a38b691ae1e2659f025aa18aa0aac1b33c0fe1b
nevans <nevans>
2007-08-16 15:38:40 +0000

    * w/ all exceptions

--- exceptions.inc.php
+++ exceptions.inc.php
@@ -41,4 +41,70 @@ class FileEmptyError extends Exception
     }
 } // end FileEmptyError 
 
+class ArgumentError extends Exception
+{
+    /**
+     * Sets up the exception. 
+     * 
+     * @param   string    $message    The error text.
+     * @param   int       $code       An error code.
+     * @access private
+     * @return void
+    */
+    public function __construct($message, $code = 0)
+    {
+        parent::__construct($message,$code);
+    }
+
+    /**
+     * Convert the exception into a string. 
+     * 
+     * @access private
+     * @return string
+    */
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message} in '{$this->file}' on line {$this->line}.\n";
+    }
+
+} // end ArgumentError
+
+class SQLError extends Exception
+{   
+    /**
+     * The offending SQL query.
+     *
+     * @access private
+     * @var string
+     */
+    public $sql;
+
+    /**
+     * Sets up the exception. 
+     * 
+     * @param   string    $message    The error text.
+     * @param   string    $sql        The query that failed. 
+     * @param   int       $code       An error code.
+     * access   private
+     * @return void
+    */
+    public function __construct($message,$sql,$code = 0)
+    {
+        parent::__construct($message,$code);
+        $this->sql = $sql;
+    }
+
+    /**
+     * Convert the exception into a string. 
+     * 
+     * @return string
+    */
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message} in '{$this->file}' on line {$this->line}. SQL specified was: {$this->sql}\n";
+    }
+
+} // end
+
+
 ?>

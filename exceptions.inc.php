adc895aea6fb73db8c2117296827ed25acb4dba3
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2008-03-16 02:01:45 -0400

    Accidentally removed SQLError. That is kind of important.

--- exceptions.inc.php
+++ exceptions.inc.php
@@ -107,4 +107,50 @@ class SQLGenerationError extends Exception
     }
 } // end SQLGenerationError
 
+/**
+ * SQLError to be thrown when a query fails. 
+ * 
+ * @uses Exception
+ * @package ActivePHP
+ * @copyright 2007 Nicholas Evans
+ * @author Nick 'Owl' Evans <owlmanatt@gmail> 
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPL v3
+ **/
+class SQLError extends Exception
+{   
+    /**
+     * The offending SQL query.
+     *
+     * @access private
+     * @var string
+     */
+    public $sql;
+
+    /**
+     * Sets up the exception. 
+     * 
+     * @param   string    $message    The error text.
+     * @param   string    $sql        The query that failed. 
+     * @param   int       $code       An error code.
+     * access   private
+     * @return void
+    */
+    public function __construct($message,$sql,$code = 0)
+    {
+        parent::__construct($message,$code);
+        $this->sql = $sql;
+    }
+
+    /**
+     * Convert the exception into a string. 
+     * 
+     * @return string
+    */
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message} in '{$this->file}' on line {$this->line}. SQL specified was: {$this->sql}\n";
+    }
+
+} // end SQLError
+
 ?>

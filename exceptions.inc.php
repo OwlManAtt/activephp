4f5a81277071d523795ecfc74bed462f2167bb20
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2008-03-16 04:01:20 -0400

    I hate exceptions.

--- exceptions.inc.php
+++ exceptions.inc.php
@@ -75,6 +75,39 @@ class FileHandleError extends Exception
 } // end FileHandleError 
 
 /**
+ * An exception to be thrown when an argument is invalid. 
+ *
+ * @package    ActivePHP 
+ * @version    Release: @package_version@
+*/
+class ArgumentError extends Exception
+{
+    /**
+     * Sets up the exception. 
+     * 
+     * @param   string    $message    The error text.
+     * @param   int       $code       An error code.
+     * @access private
+     * @return void
+    */
+    public function __construct($message, $code = 0) 
+    {
+        parent::__construct($message,$code);
+    }
+
+    /**
+     * Convert the exception into a string. 
+     * 
+     * @access private
+     * @return string
+    */
+    public function __toString() 
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message} in '{$this->file}' on line {$this->line}.\n";
+    }
+} // end ArgumentError
+
+/**
  * An exception to be thrown when there is an error in ActiveTable's SQL generation process.
  *
  * @package    ActivePHP 

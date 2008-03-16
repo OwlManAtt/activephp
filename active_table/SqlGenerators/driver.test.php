025518c638361206fcb23987f45ddea1173b08c8
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2008-03-15 23:45:35 -0400

    * Adding documentation that got lost when being moved to git...

---
+++ active_table/SqlGenerators/driver.test.php
@@ -0,0 +1,28 @@
+<?php
+require_once('interface.inc.php');
+require_once('oci.class.php');
+require_once('mysql.class.php');
+
+$query = new ActiveTable_SQL_Oracle();
+$query->addFrom('customer','eplus');
+$query->addKeys('customer',array('cust_number','company_name'),'a');
+$query->addKeys('sales_category_lookup',array('sales_category_desc'),'b');
+$query->addJoinClause('customer','sales_category_id','sales_category_lookup','sales_category_lookup','sales_category_id','inner','eplus');
+$query->addWhere('customer','state');
+$query->setSlice(1,10);
+print "== Oracle query:\n\n";
+print_r($query->getQuery('select'));
+print "\n\n";
+
+$query = new ActiveTable_SQL_MySQL();
+$query->addFrom('sale');
+$query->addKeys('sale',array('sale_id','acct','customer_name'),'a');
+$query->addKeys('sale_status',array('long_description'),'b');
+$query->addJoinClause('sale','sale_status_id','sale_status','sale_status','sale_status_id','inner');
+$query->addWhere('sale','site_state');
+$query->setSlice(0,10);
+print "== MySQL query:\n\n";
+print_r($query->getQuery('select'));
+print "\n\n";
+
+?>
